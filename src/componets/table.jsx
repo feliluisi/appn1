796765deb39caacf6689bb3506865bc1759549e3
julian <julianchisari101@gmail.com>
2023-02-09 12:19:11 -0300

    new routers

---
+++ src/componets/table.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+  } from '@chakra-ui/react'
+
+const dataTable =()=>{
+ 
+
+
+
+    return (
+
+
+
+
+
+    )
+}
