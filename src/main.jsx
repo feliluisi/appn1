c0347228ff13aeda084089ffa54a8982f83319f5
julian <julianchisari101@gmail.com>
2023-02-09 11:30:37 -0300

    new /table rute

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import App from '../src/App'
 import './index.css'
+const router = createBrowserRouter([
+  {
+    path: "/table",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <RouterProvider router={router} />
     <App />
   </React.StrictMode>,
 )
